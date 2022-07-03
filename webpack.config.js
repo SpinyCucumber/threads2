c068bc8143d2566133d91df61925a0dcc7ced31e
ethilty <spinycucumber@gmail.com>
2022-07-03 22:29:02 +0100

    Use HTML Webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './src/app.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Threads',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
 };
