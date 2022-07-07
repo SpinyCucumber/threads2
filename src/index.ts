49b9b922f85d35eed06f02bf7f3766ef2f1f4e18
ethilty <spinycucumber@gmail.com>
2022-07-07 12:59:07 +0100

    SCSS!

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { Threads } from "./threads";
 import { ThreadsRenderer } from "./renderer";
 
-// Get canvas and update canvas width/height
-const canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Import styles
+import "./style.scss";
 
-// Construct threads
-const threads = new Threads({ size: canvas.height, numThreads: 1 });
-
-// Construct renderer
-const renderer = new ThreadsRenderer({ canvas, threads });
+let canvas;
+let threads;
+let renderer;
+let lastTime;
 
 // Declare game loop
 function gameLoop(time: number): void {
@@ -23,8 +20,19 @@ function gameLoop(time: number): void {
     window.requestAnimationFrame(gameLoop);
 }
 
-// Initialize threads, renderer, and start game loop
-let lastTime = window.performance.now();
-threads.populate();
-renderer.reset();
-window.requestAnimationFrame(gameLoop);
+function start(): void {
+    // Update canvas width/height
+    canvas = document.querySelector("canvas");
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    // Construct Threads and renderer
+    threads = new Threads({ size: canvas.height, numThreads: 1 });
+    renderer = new ThreadsRenderer({ canvas, threads });
+    // Initialize threads, renderer, and start game loop
+    lastTime = window.performance.now();
+    threads.populate();
+    renderer.reset();
+    window.requestAnimationFrame(gameLoop);
+}
+
+window.addEventListener('load', start);
