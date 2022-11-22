a983f950b019fc7a6243096df3b740df3316f51e
ethilty <spinycucumber@gmail.com>
2022-11-21 20:43:29 -0600

    Add position arg to strokeCurve

--- src/canvas-renderer/renderer.ts
+++ src/canvas-renderer/renderer.ts
@@ -1,4 +1,4 @@
-import { sleep } from "../utility";
+import { Position, sleep } from "../utility";
 import { Curve } from "../utility/curve";
 
 export interface RendererOptions {
@@ -7,16 +7,24 @@ export interface RendererOptions {
 
 export class Renderer {
 
-    context: CanvasRenderingContext2D;
-    numDivisions: 4;
+    private context: CanvasRenderingContext2D;
+    private numDivisions = 3;
 
     constructor({ canvas }: RendererOptions) {
         this.context = canvas.getContext("2d");
+        this.context.lineWidth = 5.0;
+        this.context.strokeStyle = "white";
+        this.context.lineCap = "round";
     }
 
-    async strokeCurve(curve: Curve, duration: number) {
+    // TODO Cache segments?
+    async strokeCurve(position: Position, curve: Curve, duration: number) {
+        // Transform context
+        this.context.save();
+        this.context.translate(position.x, position.y);
+        // Sample curve and draw segments
         const segments = curve.sample(this.numDivisions);
-        const period = duration / segments.size;
+        const period = duration / Math.pow(2, this.numDivisions);
         for (const [a, b] of segments) {
             this.context.beginPath();
             this.context.moveTo(a.x, a.y);
@@ -24,6 +32,8 @@ export class Renderer {
             this.context.stroke();
             await sleep(period);
         }
+        // Restore context
+        this.context.restore();
     }
 
 }
